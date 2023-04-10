f4109614535714e1a00493a88447ab53d7f8f0f3
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2023-04-10 14:31:29 -0300

    Adicionei a ação de create ao projeto.

--- WebAppProjeto2023/WebAppProjeto2023/Controllers/CategoriasController.cs
+++ WebAppProjeto2023/WebAppProjeto2023/Controllers/CategoriasController.cs
@@ -23,5 +23,11 @@ namespace WebAppProjeto2023.Controllers
         {
             return View(categorias);
         }
+
+        // GET: Categorias
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
