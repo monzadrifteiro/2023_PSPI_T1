697e6ee520344558f5eae8b41decf900eefc432d
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2023-05-08 14:26:25 -0300

    Finalizei o CRUD da classe Fabricante.

--- WebAppProjeto2023/WebAppProjeto2023/Controllers/FabricantesController.cs
+++ WebAppProjeto2023/WebAppProjeto2023/Controllers/FabricantesController.cs
@@ -95,6 +95,7 @@ namespace WebAppProjeto2023.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
         }
     }
