289df3f7032ae130454482bae1cdf7d2f0945a64
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2023-05-09 15:54:16 -0300

    Iniciei o desenvolvimento da página Home do projeto.

---
+++ WebAppProjeto2023/WebAppProjeto2023/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjeto2023.Models;
+
+namespace WebAppProjeto2023.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+    }
+}
