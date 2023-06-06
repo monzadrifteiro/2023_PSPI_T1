c8a685dbb35a574eb55891f96a44019f5079ce1d
monzadrifteiro <lucas363nunes@gmail.com>
2023-06-06 15:56:23 -0300

    Ajustei a visão do Details da classe Categoria.

--- WebAppProjeto2023/WebAppProjeto2023/Controllers/CategoriasController.cs
+++ WebAppProjeto2023/WebAppProjeto2023/Controllers/CategoriasController.cs
@@ -66,7 +66,7 @@ namespace WebAppProjeto2023.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Categoria categoria = context.Categorias.Find(id);
+            Categoria categoria = context.Categorias.Where(f => f.CategoriaId == id).Include("Produtos.Fabricante").First();
             if (categoria == null)
             {
                 return HttpNotFound();
