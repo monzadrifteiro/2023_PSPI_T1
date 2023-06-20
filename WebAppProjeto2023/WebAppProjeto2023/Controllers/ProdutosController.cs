5a6e62a761718396add09cd4297d125632e0f3a0
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2023-06-20 15:53:06 -0300

    Inseri os métodos responsáveis pelo upload de imagem na classe de Produtos.

--- WebAppProjeto2023/WebAppProjeto2023/Controllers/ProdutosController.cs
+++ WebAppProjeto2023/WebAppProjeto2023/Controllers/ProdutosController.cs
@@ -78,10 +78,67 @@ namespace WebAppProjeto2023.Controllers
             return View(produto);
         }
 
+        private byte[] SetLogotipo(HttpPostedFileBase logotipo)
+        {
+            var bytesLogotipo = new byte[logotipo.ContentLength];
+            logotipo.InputStream.Read(bytesLogotipo, 0, logotipo.ContentLength);
+            return bytesLogotipo;
+        }
+
+        private void PopularViewBag(Produto produto = null)
+        {
+            ViewBag.CategoriaId = new SelectList(context.Categorias.OrderBy(b => b.Nome), "CategoriaId", "Nome", produto.CategoriaId);
+            ViewBag.FabricanteId = new SelectList(context.Fabricantes.OrderBy(b => b.Nome), "FabricanteId", "Nome", produto.FabricanteId);
+        }
+
+        public void GravarProduto(Produto produto)
+        {
+            if (produto.ProdutoId == null)
+            {
+                context.Produtos.Add(produto);
+            }
+            else
+            {
+                context.Entry(produto).State = EntityState.Modified;
+            }
+            context.SaveChanges();
+        }
+
+        private ActionResult GravarProduto(Produto produto, HttpPostedFileBase logotipo, string chkRemoverImagem)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (chkRemoverImagem != null)
+                    {
+                        produto.Logotipo = null;
+                    }
+                    if (logotipo != null)
+                    {
+                        produto.LogotipoMimeType = logotipo.ContentType;
+                        produto.Logotipo = SetLogotipo(logotipo);
+                    }
+                    //produtoServico.GravarProduto(produto);
+                    GravarProduto(produto);
+                    return RedirectToAction("Index");
+                }
+                PopularViewBag(produto);
+                return View(produto);
+            }
+            catch
+            {
+                PopularViewBag(produto);
+                return View(produto);
+            }
+        }
+
         // POST: Produtos/Edit/5
         [HttpPost]
-        public ActionResult Edit(Produto produto)
+        public ActionResult Edit(Produto produto, HttpPostedFileBase logotipo = null, string chkRemoverImagem = null)
         {
+            return GravarProduto(produto, logotipo, chkRemoverImagem);
+            /*
             try
             {
                 if (ModelState.IsValid)
@@ -96,6 +153,23 @@ namespace WebAppProjeto2023.Controllers
             {
                 return View(produto);
             }
+            */
+        }
+
+        public Produto ObterProdutoPorId(long id)
+        {
+            return context.Produtos.Where(p => p.ProdutoId == id).Include(c => c.Categoria).Include(f => f.Fabricante).First();
+        }
+
+        public FileContentResult GetLogotipo(long id)
+        {
+            //Produto produto = produtoServico.ObterProdutoPorId(id);
+            Produto produto = ObterProdutoPorId(id);
+            if (produto != null)
+            {
+                return File(produto.Logotipo, produto.LogotipoMimeType);
+            }
+            return null;
         }
 
         // GET: Produtos/Delete/5
